dfb181e8cdba63f3f461d4398d1fd820c95ad991
hiroki0456 <volleyball0456@gmail.com>
2022-04-19 23:48:07 +0900

    for statement

--- main.go
+++ main.go
@@ -13,15 +13,15 @@ import "fmt"
 
 // const Pi = 3.14
 
-const (
-	Big   = 1 << 100
-	Small = Big >> 99
-)
+// const (
+// 	Big   = 1 << 100
+// 	Small = Big >> 99
+// )
 
-func needInt(x int) int { return x*10 + 1 }
-func needFloat(x float64) float64 {
-	return x * 0.1
-}
+// func needInt(x int) int { return x*10 + 1 }
+// func needFloat(x float64) float64 {
+// 	return x * 0.1
+// }
 func main() {
 	// var c, python, java = true, false, "no!"
 	// var i, j int = 1, 2
@@ -50,7 +50,18 @@ func main() {
 	// const Truth = true
 	// fmt.Println("Go rules?", Truth)
 
-	fmt.Println(needInt(Small))
-	fmt.Println(needFloat(Small))
-	fmt.Println(needFloat(Big))
+	// fmt.Println(needInt(Small))
+	// fmt.Println(needFloat(Small))
+	// fmt.Println(needFloat(Big))
+	For()
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
+
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
 }
